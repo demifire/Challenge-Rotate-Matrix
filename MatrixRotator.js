47f4492e68c1dfb46da4222fcda315fbd2b83d17
Chaz Kondo <Chaz@2ppgn4x.local>
2018-07-14 11:49:16 -1000

    Figured out my way for both probs

--- MatrixRotator.js
+++ MatrixRotator.js
@@ -23,51 +23,53 @@ module.exports = class MatrixRotator {
     // console.log('YOYOYO', this.matrix);
     // console.log('DIS', direction);
 
-    if (direction === 'ClockWise'){
-            var thisMatrix = this.matrix;
-            var newMatrix = [];
-            var innerArr = [];
-            for (var j=0; j<thisMatrix[0].length; j) {
-              for (var i=thisMatrix.length-1; i>=0; i--) {
-                innerArr.push(thisMatrix[i][0]);
-                thisMatrix[i].shift();
-              }
-              newMatrix.push(innerArr);
-              innerArr = [];
-            }
-            this.matrix = newMatrix;
-            console.log(thisMatrix);
+    // if (direction === 'ClockWise'){
+    //         var thisMatrix = this.matrix;
+    //         var newMatrix = [];
+    //         var innerArr = [];
+    //         for (var j=0; j<thisMatrix[0].length; j) {
+    //           for (var i=thisMatrix.length-1; i>=0; i--) {
+    //             innerArr.push(thisMatrix[i][0]);
+    //             thisMatrix[i].shift();
+    //           }
+    //           newMatrix.push(innerArr);
+    //           innerArr = [];
+    //         }
+    //         this.matrix = newMatrix;
+    //         console.log(thisMatrix);
 
-
+    // }
       //My method
 
-      // if (direction === 'ClockWise'){
-      //   console.log('FUKU');
-      //   while(this.matrix[0].length>0){
-      //     innerArr = [];
-      //     for(var i=this.matrix[0].length-1;i>0;i--){
-      //       console.log('DIS BE ARR ' + i, this.matrix[i]);
-      //       var WAT = this.matrix[i].shift();
-      //       console.log(WAT);
-      //       innerArr.push(WAT);
-      //     }
-      //     rotatedMatrix.push(innerArr)
-      //   }
-      //   console.log(rotatedMatrix)
-      //   this.matrix = rotatedMatrix
-      // }
+      if (direction === 'ClockWise'){
+        let thisMatrix = this.matrix;
+        console.log('Solving Clockwise Problem');
+        console.log(thisMatrix.length);
+        while(thisMatrix[0].length>0){
+          innerArr = [];
+          for(let i=this.matrix.length-1;i>-1;i--){
+            console.log('Inner Arr is ' + i, this.matrix[i]);
+            let shifted = this.matrix[i].shift();
+            console.log(shifted);
+            innerArr.push(shifted);
+          }
+          rotatedMatrix.push(innerArr)
+        }
+        console.log(rotatedMatrix)
+        this.matrix = rotatedMatrix
+      }
 
-    }
+  
 
     if (direction === 'CounterClockWise'){
-      console.log('FUKU');
+      console.log('Solving Counter Clockwise Problem');
       while(this.matrix[0].length>0){
         innerArr = [];
-        for(var i=0;i<this.matrix.length;i++){
-          console.log('DIS BE ARR ' + i, this.matrix[i]);
-          var WAT = this.matrix[i].pop();
-          console.log(WAT);
-          innerArr.push(WAT);
+        for(let i=0;i<this.matrix.length;i++){
+          console.log('Inner Arr is ' + i, this.matrix[i]);
+          let popped = this.matrix[i].pop();
+          console.log(popped);
+          innerArr.push(popped);
         }
         rotatedMatrix.push(innerArr)
       }
